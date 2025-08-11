c0b3d2e43da4a810053c069c0aeb57883b7637ca
kawalesumit <kawalesumit320@gmail.com>
2025-08-11 07:55:50 +0530

    Number guessing game

--- Main.java
+++ Main.java
@@ -1,19 +1,41 @@
+import java.util.Random;
 import java.util.Scanner;
 
-public class Main{
+public class Main {
+    public static void main(String[] args){
 
-    public static  void main(String[] args){
+        Random random = new Random();
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter Height Of Right Angle Triangle: ") ;
-        double  height = scanner.nextDouble();
 
-        System.out.print("Enter Base Of Right Angle Triangle: ");
-        double base = scanner.nextDouble();
+        int randomNumber = random.nextInt(1 , 11);
+        int attempt = 0;
+        int guess;
+
+        System.out.println("Welcome To Number Guessing Game");
+        System.out.println("Guess Number Between 1 to 10");
+
+        do {
+            System.out.print("Enter Your guess: ");
+            guess = scanner.nextInt();
+            attempt++;
+
+            if (randomNumber < guess){
+                System.out.println("Too High! Try Again");
+            }
+            else if (randomNumber > guess) {
+                System.out.println("Too Low! Try Again");
+
+            }
+            else {
+                System.out.println("Correct You Have Won 🎊");
+            }
 
 
-        double area = base * height * (1.0/2.0);
+        }while (guess != randomNumber);
 
-        System .out .print("The Area of Above Right Angle Triangle is " + area ) ;
-        scanner.close();
+            System.out.println("No. of Attempts: " + attempt);
+
+            scanner.close();
+
     }
 }
